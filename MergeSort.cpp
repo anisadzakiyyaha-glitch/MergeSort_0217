6af1772695598bb0614cdcb29a96550450179d5c
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-03-31 11:28:08 +0700

    Tambah base case + mid

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -38,3 +38,10 @@ void mergeSort(int low, int high)
     {
        return;
     }
+       int mid = (low + high) / 2;
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+
+    int i = low;
+    int j = mid + 1;
+    int k = low;
